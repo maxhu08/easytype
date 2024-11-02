05517c51ceca48fa76c66588fb73639f9fa80e43
maxhu08 <contact@maxhu.dev>
2024-11-01 21:17:45 -0500

    improve

--- src/scripts/listen.ts
+++ src/scripts/listen.ts
@@ -9,7 +9,24 @@ export const listenToEvents = () => {
 
   testInput.addEventListener("focus", (e) => focusTestInput(e));
 
+  let wordIndex = 0;
   document.addEventListener("keydown", (e) => {
-    if (e.key === " ") tryFocusTestInput(e);
+    if (e.key === "Escape") unfocusTestInput();
+    if (e.key === " ") {
+      e.preventDefault();
+      tryFocusTestInput(e);
+
+      const wordSpanEl = document.getElementById(`w-${wordIndex}`) as HTMLSpanElement;
+
+      if (testInput.value === wordSpanEl.textContent) {
+        // prettier-ignore
+        (document.getElementById(`w-${wordIndex}`) as HTMLSpanElement).classList.remove("bg-neutral-500");
+        wordIndex++;
+        // prettier-ignore
+        (document.getElementById(`w-${wordIndex}`) as HTMLSpanElement).classList.add("bg-neutral-500");
+
+        testInput.value = "";
+      }
+    }
   });
 };
--- src/scripts/set-text.ts
+++ src/scripts/set-text.ts
@@ -3,10 +3,17 @@ import { testText } from "./ui";
 
 export const setTest = () => {
   let testContent = "";
-  for (let i = 0; i < 20; i++) {
-    testContent += english.words[Math.round(Math.random() * english.words.length)];
-    if (i !== 19) testContent += " ";
 
-    testText.textContent = testContent;
+  for (let i = 0; i < 20; i++) {
+    const word = english.words[Math.floor(Math.random() * english.words.length)];
+    const span = document.createElement("span");
+    span.id = `w-${i}`;
+    span.textContent = word;
+
+    testText.appendChild(span);
+
+    if (i !== 19) testText.appendChild(document.createTextNode(" "));
   }
+
+  (document.getElementById(`w-${0}`) as HTMLSpanElement).classList.add("bg-neutral-500");
 };
