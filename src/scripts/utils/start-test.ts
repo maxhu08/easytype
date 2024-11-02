da759e178307216250007ecc5606fcd44c13b5bb
maxhu08 <contact@maxhu.dev>
2024-11-02 14:00:04 -0500

    fix issue with timer still going when restarting test

--- src/scripts/utils/start-test.ts
+++ src/scripts/utils/start-test.ts
@@ -1,13 +1,14 @@
 import { resultsCard, testCard, testInput } from "../ui";
 import { tryFocusTestInput } from "./input";
 import { setTest } from "./set-text";
-import { resetTimer } from "./start-timer";
+import { resetTimer, stopTimer } from "./start-timer";
 import { setWordIndex } from "./word-index";
 
 export const handleStartTest = () => {
   setTest();
   setWordIndex(0);
   testInput.value = "";
+  stopTimer();
   resetTimer();
 
   testCard.classList.replace("hidden", "block");
