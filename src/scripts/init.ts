5df769afb1e8d545224b4e89632834f80b7f83be
maxhu08 <contact@maxhu.dev>
2024-11-03 14:26:26 -0600

    add more options stuff

--- src/scripts/init.ts
+++ src/scripts/init.ts
@@ -1,15 +1,22 @@
 import { listenToEvents } from "./listen";
 import { optionsButton, optionsDoneButton, restartTestButton } from "./ui";
+import { getConfig } from "./utils/config/config-helpers";
+import { fillOptions } from "./utils/config/fill-options";
 import { handleAllButtonSwitches } from "./utils/config/handle-switch";
 import { closeOptions, openOptions } from "./utils/open-options";
 import { getTestInfo } from "./utils/set-test-info";
 import { handleStartTest } from "./utils/start-test";
 
+const config = getConfig();
+
 handleStartTest();
 const testInfo = getTestInfo();
+
 listenToEvents(testInfo);
 
 restartTestButton.onclick = () => handleStartTest();
 optionsButton.onclick = () => openOptions();
 optionsDoneButton.onclick = () => closeOptions();
+
 handleAllButtonSwitches();
+fillOptions(config);
---
+++ src/scripts/utils/config/fill-options.ts
@@ -0,0 +1,24 @@
+import type { Config } from "../../types";
+import {
+  optionsCurrentWordIndicatorAnimationsOffButton,
+  optionsCurrentWordIndicatorAnimationsOnButton,
+  optionsTestTypeWordsButton,
+  optionsWords100Button,
+  optionsWords10Button,
+  optionsWords200Button,
+  optionsWords25Button,
+  optionsWords50Button
+} from "../../ui";
+
+export const fillOptions = (config: Config) => {
+  if (config.testInfo.type === "words") optionsTestTypeWordsButton.click();
+
+  if (config.testInfo.words === 10) optionsWords10Button.click();
+  else if (config.testInfo.words === 25) optionsWords25Button.click();
+  else if (config.testInfo.words === 50) optionsWords50Button.click();
+  else if (config.testInfo.words === 100) optionsWords100Button.click();
+  else if (config.testInfo.words === 200) optionsWords200Button.click();
+
+  if (config.currentWordIndicatorAnimations === "on") optionsCurrentWordIndicatorAnimationsOnButton.click();
+  else if (config.currentWordIndicatorAnimations === "off") optionsCurrentWordIndicatorAnimationsOffButton.click();
+};
